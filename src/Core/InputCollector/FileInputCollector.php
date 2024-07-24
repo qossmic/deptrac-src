f8c35bcc0e25242985c3c731695c6ba0126690ed
Nikolaj Boiko <boiko@next-kraftwerke.de>
2024-07-24 09:33:20 +0200

    Add sortByName to FileInputCollector

--- src/Core/InputCollector/FileInputCollector.php
+++ src/Core/InputCollector/FileInputCollector.php
@@ -63,7 +63,8 @@ final class FileInputCollector implements InputCollectorInterface
                 ->followLinks()
                 ->ignoreUnreadableDirs()
                 ->ignoreVCS(true)
-                ->notPath($this->excludedFilePatterns);
+                ->notPath($this->excludedFilePatterns)
+                ->sortByName();
 
             $customFilterIterator = $finder->getIterator();
         } catch (LogicException|DirectoryNotFoundException $exception) {
