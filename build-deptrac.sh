ecf66de69246883eaf8db6f37918e48e50aff6d1
Gennadi McKelvey <gennadijanzen@icloud.com>
2024-11-21 21:14:47 +0100

    update build

--- build-deptrac.sh
+++ build-deptrac.sh
@@ -4,8 +4,14 @@ DEPTRAC_DIR="${DEPTRAC_DIR:-../deptrac-scoped}"
 BUILD_DIR=build
 BUILD_TMP=${BUILD_DIR}/deptrac-build
 PHP='docker compose exec -u 1000 deptrac php -d memory_limit=-1'
+CONTAINER=docker compose exec -u 1000 deptrac bash
+# PHP='php'
 SCOPER=$BUILD_DIR/php-scoper.phar
 BOX=$BUILD_DIR/box.phar
+
+echo $DEPTRAC_DIR
+echo $BUILD_DIR
+echo $BUILD_TMP
 
 info()
 {
@@ -26,19 +32,21 @@ $PHP $SCOPER add-prefix --force --config scoper.inc.php --working-dir . --output
 $PHP $BOX compile
 
 # info "sign phar"
-gpg --detach-sign --armor --local-user ${USER} --output $BUILD_TMP/deptrac.phar.asc $BUILD_TMP/deptrac.phar
+gpg --detach-sign --armor --local-user 974E9033414D7F2BC9FE1E6AD4F06E96D1BD037B --output $BUILD_TMP/deptrac.phar.asc $BUILD_TMP/deptrac.phar
 gpg --verify $BUILD_TMP/deptrac.phar.asc $BUILD_TMP/deptrac.phar
 
 info "Dump Composer Autoloader"
 $PHP /usr/bin/composer dump-autoload --working-dir $BUILD_TMP -a --no-dev
 
 info "Copy package templates"
-cp -r $BUILD_DIR/template/* *.md mkdocs.yml docs -t $BUILD_TMP
-cp -r $BUILD_DIR/template/.* -t $BUILD_TMP
+$CONTAINER cp -rv $BUILD_DIR/template/* *.md mkdocs.yml docs -t $BUILD_TMP
+$CONTAINER cp -rv $BUILD_DIR/template/.github -t $BUILD_TMP
+$CONTAINER cp -rv $BUILD_DIR/template/.gitignore -t $BUILD_TMP
 
+# exit 1;
 info "Copy build into deptrac distrubtion repository"
-cp -r $BUILD_TMP/* $DEPTRAC_DIR 
-cp -rv $BUILD_TMP/.* $DEPTRAC_DIR 
+cp -rv $BUILD_TMP/* $DEPTRAC_DIR 
+# cp -rv $BUILD_TMP/.* $DEPTRAC_DIR 
 
 # info "Git commit changes"
 # echo "Update $(date)" > git_commit_message.txt
