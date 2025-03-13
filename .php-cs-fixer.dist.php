2054340539c17a8bd445def08f6778b1db18f075
Gennadi McKelvey <gennadijanzen@icloud.com>
2025-03-13 09:13:41 +0100

    chore(cs): run php-cs-fixer in parallel
    and add php-cs-config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,21 @@
 <?php
 
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
 $finder = (new PhpCsFixer\Finder())
-    ->in([__DIR__.'/config', __DIR__.'/src', __DIR__.'/tests'])
+    ->in([
+        __DIR__.'/config',
+        __DIR__.'/src',
+        __DIR__.'/tests'
+    ])
     ->exclude('Fixtures')
-    ->append([__DIR__.'/deptrac.php']);
+    ->append([
+        __DIR__.'/deptrac.php',
+        __DIR__.'/'.__FILE__,
+    ]);
 
 return (new PhpCsFixer\Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRiskyAllowed(true)
     ->setCacheFile('./.cache/php-cs-fixer.cache')
     ->setRules([
