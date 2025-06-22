a2cc889d7b9e056e48fe52bfd881479bc66d3d99
Dmitrij <dimabradima@gmail.com>
2025-06-22 15:24:43 +0300

    [Typo] Update BoolConfig.php

--- src/Contract/Config/Collector/BoolConfig.php
+++ src/Contract/Config/Collector/BoolConfig.php
@@ -21,11 +21,11 @@ final class BoolConfig extends CollectorConfig
      * @param array<CollectorConfig> $must
      * @param array<CollectorConfig> $mostNot
      */
-    public static function create(array $must = [], array $mostNot = []): self
+    public static function create(array $must = [], array $mustNot = []): self
     {
         return (new self())
             ->must(...$must)
-            ->mustNot(...$mostNot)
+            ->mustNot(...$mustNot)
         ;
     }
 
